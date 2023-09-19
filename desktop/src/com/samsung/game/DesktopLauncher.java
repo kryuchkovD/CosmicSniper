59a657907d69301a93f787b06091e56b6ad6b9f1
Дмитрий <ddmkr@yandex.ru>
2023-09-19 22:32:41 +0300

    fixed bug

--- desktop/src/com/samsung/game/DesktopLauncher.java
+++ desktop/src/com/samsung/game/DesktopLauncher.java
@@ -10,7 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setWindowedMode(720,1280);
-		config.setTitle("SpaceShoter");
+		config.setTitle("CosmicSniper");
 		new Lwjgl3Application(new Main(), config);
 
 		
